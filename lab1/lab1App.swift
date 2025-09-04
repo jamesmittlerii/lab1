4541f0d7251eb18cb5f1d700434a75dd5c1b20e7
James Mittler II <jamesmittlerii@my.smccd.edu>
2025-09-03 19:08:26 -0700

    Initial Commit

---
+++ lab1/lab1App.swift
@@ -0,0 +1,17 @@
+//
+//  lab1App.swift
+//  lab1
+//
+//  Created by cisstudent on 9/3/25.
+//
+
+import SwiftUI
+
+@main
+struct lab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
