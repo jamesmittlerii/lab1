866daa4e09ce1c69265286c71f76a62bd2f7d564
James Mittler II <jamesmittlerii@my.smccd.edu>
2025-09-21 07:45:28 -0700

    Update lab1App.swift

--- lab1/lab1App.swift
+++ lab1/lab1App.swift
@@ -1,12 +1,21 @@
-//
-//  lab1App.swift
-//  lab1
-//
-//  Created by cisstudent on 9/3/25.
-//
+/**
+
+ * __Lab 1__
+ * Jim Mittler and Alex Tererin
+ * 21 September 2025
+
+ This program loads dog descriptions and images.
+ We display a grid of the dog images and show a description when the image is clicked.
+
+ _Italic text_
+ __Bold text__
+ ~~Strikethrough text~~
+
+ */
 
 import SwiftUI
 
+// main launch
 @main
 struct lab1App: App {
     var body: some Scene {
